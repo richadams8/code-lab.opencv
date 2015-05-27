a35718e91c334c2a1a2b6a5923b13981d756c5c4
Richard Adams <radams@jive.com>
2015-05-27 10:49:57 -0600

    Getting warmer

--- examples/open_and_display_image.py
+++ examples/open_and_display_image.py
@@ -4,6 +4,8 @@ import numpy as np
 import cv2
 from matplotlib import pyplot as plt
 
-img = cv2.imread("../samples/polycom_simple.jpg")
-plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+img_bgr = cv2.imread("../samples/polycom_simple.jpg")
+img_rgb = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2RGB)
+plt.imshow(img_rgb, cv2.COLOR_BGR2RGB)
+plt.axis('off')
 plt.show()
