ba5d26ebf2bb5e18ae0569bd4cd4903add65f4f5
Richard Adams <radams@jive.com>
2015-05-27 20:24:24 -0600

    Filling out example for class

--- examples/extract_text.py
+++ examples/extract_text.py
@@ -4,9 +4,14 @@ import numpy as np
 import cv2
 from matplotlib import pyplot as plt
 
-print "hello world"
-
 img_bgr = cv2.imread("../samples/polycom_simple.jpg")
-plt.imshow(img_bgr)
-plt.show()
+img_rgb = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2RGB)
 
+f, (top, bottom) = plt.subplots(2, 1, True)
+
+top.imshow(img_bgr, aspect='auto')
+bottom.imshow(img_rgb, aspect='auto')
+
+plt.tight_layout()
+
+plt.show()
